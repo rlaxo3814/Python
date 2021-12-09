277dbacb1b228a370214821072bf2184eaf6795d
김태형 <91590335+rlaxo3814@users.noreply.github.com>
2021-12-09 19:31:12 +0900

    Python Practice2

---
+++ step2.py
@@ -0,0 +1,9 @@
+import random
+
+def makeRandomNumber():
+    #rnd = random.random() # 0.0<= rnd <1.0
+
+    for _ in range(10):
+        rnd2 = random.randint(1,3)#random.randrange(1, 3)
+        print(rnd2)
+    pass
