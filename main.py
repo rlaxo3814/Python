385ac41b6f0f86d8d6b64879294b78b9fd900bbe
김태형 <91590335+rlaxo3814@users.noreply.github.com>
2021-12-16 21:01:38 +0900

    Python Practice8

---
+++ main.py
@@ -0,0 +1,46 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from classOne import step1
+from classTwo import step1 as cts1
+from classTwo import step2 as cts2
+from classThree import step31
+from classThree import classModule
+from classThree.classModule import MemberVO
+from classThree.funcModule import sendURL
+
+sendURL()
+
+#vo = classModule.MemberVO('admin','1234','관리자')
+#print("{0} , {2} , {1}".format(vo.id, vo.name, vo.pwd))
+#print(vo)
+#member = MemberVO('user','1234','사용자')
+#print(member)
+#print(step31.hello())
+#step31.getCurrentDate()
+#test = step31.Test()
+#print(test.id)
+#cts2.testFor()
+#year = int(input('please input year >>> '))
+#month = int(input('please input month >>> '))
+#cts1.viewMonth(year,month)
+#cts1.testDict2()
+#step2.makeRandomNumber()
+#step1.test()
+#step1.test1(10,20)
+#step1.controlStatement()
+#step1.viewCalendar()
+#result = step1.isLeapYear()
+#print(result)
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
